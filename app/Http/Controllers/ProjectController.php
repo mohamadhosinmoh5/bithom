68742091a875b0e57bf789b7e59dcf0094b9050c
narges <narges.80.hoseyni@gmail.com>
2024-08-08 09:44:41 +0330

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -61,32 +61,11 @@ class ProjectController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'mobile' => 'required|numeric|digits:11',
-            'supply_status' => 'required|string|max:255',
             'title' => 'required|string|max:255',
-            'city' => 'required|string|max:255',
-            'maker'=> 'required|string|max:255',
-            'start_time' => 'required|string|max:255',
-            'end_time' => 'required|string|max:255',
             'price' => 'required|numeric',
             'address'=> 'required|string',
-            'lat' => 'required|numeric',
-            'long' => 'required|numeric',
-            'area' => 'required|string|max:255',
-            'projectType_id' => 'required|numeric',
             'meterage' => 'required|numeric',
-            'floor' => 'required|numeric',
-            'base_price' => 'required|numeric',
-            'project_info' => ($request->project_info !== null) ? 'required|string' : '',
-            'investment_status' => 'required|string|max:255',
-            'remaining_meterage' => 'required|numeric',
-            'start_price_investment' => 'required|numeric',
-            'supply_status_code' => 'required|numeric',
-            'baseTitle' => 'required|string|max:255',
-            'currentPrice'=> 'required|numeric',
-            'main_img_id'=> 'required|numeric',
-            'store'  => ($request->store !== null) ? 'required|numeric' : '',
-            'parking' => ($request->parking !== null) ? 'required|numeric' : '',
-            'room'  => ($request->room !== null) ? 'required|numeric' : '',
+            'attechment_file_id' => 'required|numeric',
         ]);
 
         if ($validator->fails()) {
@@ -99,48 +78,17 @@ class ProjectController extends Controller
         $user = User::where('mobile', $request->mobile)->first();
         if($user){
             $project = Project::create([
-                'supply_status' => $request->supply_status,
                 'title' => $request->title,
-                'city' => $request->city,
-                'maker' => $request->maker,
-                'start_time' => $request->start_time,
-                'end_time' => $request->end_time,
                 'price' => $request->price,
                 'address' => $request->address,
-                'lat' => $request->lat,
-                'long' => $request->long,
-                'area' => $request->area,
-                'projectType_id' => $request->projectType_id,
                 'meterage' => $request->meterage,
-                'floor' => $request->floor,
-                'base_price' => $request->base_price,
-                'project_info' => $request->project_info,
-                'investment_status' => $request->investment_status,
-                'remaining_meterage' => $request->remaining_meterage,
-                'start_price_investment' => $request->start_price_investment,
-                'supply_status_code' => $request->supply_status_code,
-                'baseTitle' => $request->baseTitle,
-                'currentPrice' => $request->currentPrice,
-                'main_img_id' => $request->main_img_id,
+                'attechment_file_id' => $request->attechment_file_id,
             ]);
-
-            $unit = Unit::create([
-                'project_id' => $project->id,
-                'title' => $project->title,
-                'price' => $project->price,
-                'projectType_id' => $project->projectType_id,
-                'meterage' => $project->meterage,
-                'remaining_meterage' => $project->remaining_meterage,
-                'store' => $request->store,
-                'parking' => $request->parking,
-                'room' => $request->room,
-            ]);
-            $project = Project::findOrFail($project->id)->unit;
-            $project  = $unit->id->save();
 
             return response()->json([
                 'status' => true,
                 'message' => 'پروژه ثبت شد.',
+                'project' => $project
             ], 201);
         }
         return response()->json([
@@ -204,4 +152,5 @@ class ProjectController extends Controller
 
 
     // }
+
 }
